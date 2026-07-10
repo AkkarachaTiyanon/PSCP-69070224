48bc9d97c063c79827803237e29ec675123c19d7
RangsimanChuthap <stupidhalppp@gmail.com>
2026-07-10 12:25:59 +0700

    j,mhn,

---
+++ 3025/main.py
@@ -0,0 +1,17 @@
+"""ss"""
+
+
+def main():
+    """ssdfr"""
+    month = int(input())
+    day = int(input())
+    if month <= 3:
+        
+    else:
+        elif month <= 6:
+
+        elif month <= 9:
+
+        elif month <= 12:
+
+main()
