53fb143ecb9182c52702d9e522fa70f9c672b4d3
AkkarachaTiyanon <69070224@kmitl.ac.th>
2026-09-12 20:58:08 +0700

    no more

---
+++ 3290/main.py
@@ -0,0 +1,16 @@
+"""x"""
+def print_turn_signal(k, n):
+    """w"""
+    mid = n // 2
+    for i in range(n):
+        indent = abs(mid - i)
+        print(" " * indent + "*" * k)
+
+
+def main():
+    """z"""
+    k = int(input())
+    n = int(input())
+    print_turn_signal(k, n)
+
+main()
